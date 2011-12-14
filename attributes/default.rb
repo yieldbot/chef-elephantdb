8fe6bd964ce84c0d12ddef9bc66530140cc90660
Dave White <dwhite@yieldbot.com>
2011-12-14 16:02:57 -0500

    Add more default attributes and configure global/local config with them.

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,4 +6,7 @@ default['elephantdb']['data_dir'] = '/mnt/elephantdb'
 default['elephantdb']['log_dir'] = '/var/log/elephantdb'
 default['elephantdb']['src_dir'] = '/usr/local/elephantdb'
 default['elephantdb']['src_repo'] = 'git://github.com/nathanmarz/elephantdb.git'
+default['elephantdb']['port'] = 3578
+default['elephantdb']['max_online_download_rate'] = 1024
+default['elephantdb']['update_interval'] = 60
 
